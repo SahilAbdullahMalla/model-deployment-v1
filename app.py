aece14240892b3cd3080ece67562e190b0a6ca3e
SahilAbdullahMalla <sahilabdullahmalla@gmail.com>
2025-07-03 12:10:37 +0530

    app-model-update

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
 import pickle 
+import os
 
-tokenizer =pickle.load(open(r"C:\Users\abdul\projects\models\cv.pkl","rb"))
-model=pickle.load(open(r"C:\Users\abdul\projects\models\clf.pkl","rb"))
+# Get the directory where app.py is located
+BASE_DIR = os.path.dirname(__file__)
+
+# Build full paths to the model files
+cv_path = os.path.join(BASE_DIR, "models", "cv.pkl")
+clf_path = os.path.join(BASE_DIR, "models", "clf.pkl")
+
+# Load the models
+with open(cv_path, "rb") as f:
+    tokenizer = pickle.load(f)
+
+with open(clf_path, "rb") as f:
+    model = pickle.load(f)
+
 app = Flask(__name__)
 
 @app.route("/")
